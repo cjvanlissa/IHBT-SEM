f71a5e041097c4a97f7099f80a4491a274a989b9
cjvanlissa <c.j.vanlissa@uu.nl>
2020-09-23 10:51:22 +0200

    Fix simulated data

--- prepare_data.R
+++ prepare_data.R
@@ -5,4 +5,15 @@
 
 library(worcs)
 df <- read.csv("data.csv", stringsAsFactors = FALSE)
+df <- df[, c("sex", grep("^a.sup", names(df), value = TRUE), grep("de", names(df), value = TRUE))]
 closed_data(df)
+
+# Simulate data from the lavaan model, otherwise lavaan does not converge on simulated data
+source("manuscript/model_syntax.R")
+library(lavaan)
+model <- sem(syntax, data = df, missing = "fiml", group = "sex")
+df_sim <- simulateData(partable(model), sample.nobs = c(282, 215))
+df_sim$sex <- factor(df_sim$group, labels = c("Boy", "Girl"))
+df_sim$group <- NULL
+write.csv(df_sim, "synthetic_df.csv", row.names = FALSE)
+worcs:::store_checksum("synthetic_df.csv", entry_name = "synthetic_df.csv")
